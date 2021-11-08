9d045546223b2e7c6d55bda26b9e1acb25e1d011
David Van Goidtsnoven <vangoidtsnovendavid@gmail.com>
2021-11-08 16:48:45 +0100

    push it

---
+++ array.php
@@ -0,0 +1,13 @@
+<?php
+$me = array(
+    'firstname' => 'David',
+    'age' => 41,
+    'season' => 'Summer',
+    'is_like_soccer' => true
+);
+
+echo '<pre>';
+print_r($me);
+echo '</pre>';
+
+// To continue : multidimentionnal array
